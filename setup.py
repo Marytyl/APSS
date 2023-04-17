e2d5c2cfd73ffbdf67e7301cf8f6549ef2f61d31
benjaminfuller <benjamin.w.fuller@gmail.com>
2023-04-17 12:48:16 -0400

    using map to avoid third loop

--- setup.py
+++ setup.py
@@ -1,18 +1,55 @@
 import numpy as np
 import unireedsolomon as rs
 import map as m
+import multiprocessing as mp
+from joblib import Parallel, delayed
+
+
+def eq_matrix_one_col(eq_mat,j, lsh_list, row):
+    for i in range(1, row):
+        arg_min = 0
+        for k in range(i):
+            if lsh_list[k][j] == lsh_list[i][j]:
+                arg_min = k + 1
+                break
+        eq_mat[i][j] = arg_min
 
 def gen_eq_matrix(M, n, lsh_list):
+    parallel = 1
     row, col = M, n
     eq_mat = [[0 for i in range(col)] for j in range(row)]
-    for i in range(1, row):
-        for j in range(col):
-            arg_min = 0
-            for k in range(i):
-                if lsh_list[k][j] == lsh_list[i][j]:
-                    arg_min = k+1
-                    break
-            eq_mat[i][j] = arg_min
+
+    lsh_dict = {}
+    for j in range(col):
+        for i in range(1, row):
+            key = str(j)+", "+str(lsh_list[i][j])
+            if key in lsh_dict:
+                eq_mat[i][j] = lsh_dict[key]
+            else:
+                lsh_dict[key] = i
+
+
+    # if parallel:
+    #     self.subtrees = Parallel(n_jobs=16)(
+    #         delayed(eq_matrix_one_col)(eq_mat, j, lsh_list, row)
+    #         for j in range(col))
+    #     # for subtree_iter in self.subtrees:
+    #     #     print("total depth "+str(subtree_iter.depth))
+    #     self.total_nodes = sum([st.num_nodes for st in self.subtrees])
+    # else:
+    #     for j in range(col):
+    #         for i in range(1, row):
+    #             arg_min = 0
+    #             for k in range(i):
+    #                 if lsh_list[k][j] == lsh_list[i][j]:
+    #                     arg_min = k+1
+    #                     break
+    #             eq_mat[i][j] = arg_min
+    # pool = multiprocessing.Pool(processes=16)
+    # pool.map(eq_matrix_one_col, args = (range(10), lsh_list, row))
+    # pool.close()
+    # pool.join()
+    #
     return eq_mat
 
 def is_valid_eq(eq_mat, k):
