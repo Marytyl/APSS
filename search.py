f849d500d91479ac4582e39d6931840eb8097d55
Maryam Rezapour <maryamrezapour+github@gmail.com>
2023-04-25 13:05:56 -0400

    Update Search

--- search.py
+++ search.py
@@ -1,23 +1,43 @@
-from map import map as m
 import unireedsolomon as rs
-from main import compute_eLSH
 
+from map import map as m
 
-def search_query(q, lsh_list, k):
-    n = len(lsh_list)
-    l = compute_eLSH(q)
-    c = []
+# def search_query(l_query, lsh_list, k):
+#     n = len(lsh_list[0])
+#
+#     c = []
+#     erase = n
+#     for j in range(n):
+#         c[j] = m.retrieve(j, l_query(j))
+#         if c[j] is not None:
+#             erase -= 1
+#     if n-erase > k:
+#         return None
+#     codeword = ""
+#     for j in range(1, n):
+#         codeword += c[j]
+#     coder = rs.RSCoder(n, k)
+#     dec = coder.decode(codeword)
+#     print(dec[0])
+
+def search_query_dict(l_q, lsh_list, k, dict):
+    n = len(lsh_list[0])
+    c = [None for i in range(n+1)]
     erase = n
     for j in range(n):
-        c[j] = m.retrieve(j, l(j))
-        if c[j] is not None:
+        c[j+1] = dict[str(j) + ", " + str(l_q[j])]
+        if c[j+1] is not None:
             erase -= 1
-    if n-erase > k:
-        return None
+    if n - erase < k:
+        return ("No match")
     codeword = ""
-    for j in range(1, n):
-        codeword += c[j]
+    for char in c:
+        if char is not None:
+            codeword += char
+        else:
+            codeword += ""
     coder = rs.RSCoder(n, k)
     dec = coder.decode(codeword)
-    print(dec[0])
+    return(dec[0])
+
 
