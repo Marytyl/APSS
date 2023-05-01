ad7ccb7740cceb4807841e8cfc65c8a509c571ad
Ben Fuller <ben@rigert.engr.uconn.edu>
2023-05-01 11:31:19 -0400

    fixing million script

--- params_million.sh
+++ params_million.sh
@@ -4,5 +4,5 @@ for ((i=1; i<=10; i++))
 do
     python3 main.py --dataset_size=1000000 --lsh_size=28 --nb_eLSHes=1343 --nb_matches_needed=26 &> million9.txt
 done
-python3 main.py --dataset_size=1000000 --lsh_size=31 --nb_eLSHes=10738 --nb_matches_needed=28 > million85.txt
+python3 main.py --dataset_size=1000000 --lsh_size=31 --nb_eLSHes=10738 --nb_matches_needed=25 > million85.txt
 
