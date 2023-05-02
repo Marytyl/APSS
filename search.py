c4eabf7da4084abd2b0dc443c0cd7515aabe208f
Maryam Rezapour <maryamrezapour+github@gmail.com>
2023-05-02 14:28:20 -0400

    Update search_query_dict

--- search.py
+++ search.py
@@ -1,5 +1,7 @@
 import unireedsolomon as rs
 
+#from reedsolo import RSCodec, ReedSolomonError
+
 from map import map as m
 
 # def search_query(l_query, lsh_list, k):
@@ -25,19 +27,23 @@ def search_query_dict(l_q, lsh_list, k, dict):
     c = [None for i in range(n+1)]
     erase = n
     for j in range(n):
-        c[j+1] = dict[str(j) + ", " + str(l_q[j])]
+        if str(j) + ", " + str(l_q[j]) in dict:
+            c[j+1] = dict[str(j) + ", " + str(l_q[j])]
         if c[j+1] is not None:
             erase -= 1
     if n - erase < k:
         return ("No match")
     codeword = ""
+    print(c)
     for char in c:
         if char is not None:
             codeword += char
         else:
             codeword += ""
     coder = rs.RSCoder(n, k)
-    dec = coder.decode(codeword)
+    # coder = RSCodec(n)
+    dec= coder.decode(codeword)
+
     return(dec[0])
 
 
