a858b8151100e180b08b82fa8abd626dece9791d
Maryam Rezapour <maryamrezapour+github@gmail.com>
2023-05-02 14:26:33 -0400

    generating queries with error

--- main.py
+++ main.py
@@ -16,13 +16,56 @@ from b4_objs import node_data, Iris, to_iris
 from setup import gen_eq_matrix, is_valid_eq, sample_codes, gen_dict, gen_eq_matrix_parallel
 from search import search_query_dict
 
+def sample_errors(vector_size):
+    mean_same = 0.05
+    stdev_same = 0.056
+
+    # compute n using degrees of freedom formula
+    n = (mean_same * (1 - mean_same)) / (stdev_same ** 2)
+    p = mean_same
+    # print("p = " + str(p) + " and n = " + str(math.ceil(n)))
+
+    error_fraction = scipy.stats.binom.rvs(math.ceil(n), p) / n
+    # print(error_fraction)
+    nb_errors = round(vector_size * error_fraction)
+    return nb_errors, round(error_fraction, 3)
 
 def build_rand_dataset(M, vec_size, t, show_hist = False):
     dataset = []
+    queries = []
+    errors_table = []
     for i in range(M):
         feature = [random.getrandbits(1) for i in range(vec_size)]
         dataset.append(feature)
-    return dataset
+
+    for i in range(50):
+        query = dataset[i][:]  # need to be careful to copy value ! (keep the [:] !!)
+
+        # sample errors from distribution
+        nb_errors, fraction = sample_errors(vec_size)
+        # print("Errors from normal distribution : " + str(nb_errors))
+        errors_table.append(nb_errors)
+        # randomly sample error bits to be inverted
+        error_bits = random.sample(range(vec_size), nb_errors)
+        for b in error_bits:
+            query[b] = (query[b] + 1) % 2
+
+        queries.append(query)
+
+        # if show_hist == 1:
+        #     for j in range(0, len(dataset)):
+        #         queries[j] = []
+        #         for i in range(0, 100):
+        #             nb_errors, fraction = sample_errors(n)
+        #             temp_query = dataset[j].copy()
+        #             error_bits = random.sample(range(n), nb_errors)
+        #             for b in error_bits:
+        #                 temp_query[b] = (temp_query[b] + 1) % 2
+        #             queries[j].append(temp_query)
+        #     build_show_histogram(dataset, queries)
+        # # print(errors_table)
+        # plt.plot(errors_table)
+    return dataset, queries
 
 # def put_elements_map(element, output):  # puts elements in hash_to_iris
 #     for index, h in enumerate(output):
@@ -60,27 +103,27 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', help="Dataset to test.", type=str, default='rand')
-    parser.add_argument('--dataset_size', help="Size of dataset to test.", type=int, default=1000000)
-    parser.add_argument('--lsh_size', help="LSH output size.", type=int, default=28)
+    parser.add_argument('--dataset_size', help="Size of dataset to test.", type=int, default=1000)
+    parser.add_argument('--lsh_size', help="LSH output size.", type=int, default=17)
     parser.add_argument('--internal_bf_fp', help="LSH output size.", type=float, default=.1)
     parser.add_argument('--root_bf_fp', help="LSH output size.", type=float, default=.0001)
-    parser.add_argument('--nb_eLSHes', help="Number of eLSHes.", type=int, default=1343)
+    parser.add_argument('--nb_eLSHes', help="Number of eLSHes.", type=int, default=255)
     parser.add_argument('--show_histogram', help="Show histogram for tested dataset.", type=int, default=0)
     parser.add_argument('--same_t', help="Avg distance between vectors from same class.", type=float, default=0.3)
     parser.add_argument('--diff_t', help="Avg distance between vectors from different class.", type=float, default=0.4)
     parser.add_argument('--nb_queries', help="Number of queries.", type=int, default=356)
-    parser.add_argument('--nb_matches_needed', help="Number of needed matches.", type=int, default=26)
+    parser.add_argument('--nb_matches_needed', help="Number of needed matches.", type=int, default=32)
     args = parser.parse_args()
 
     M = args.dataset_size  # dataset size
     n = args.nb_eLSHes  # number of eLSHes to calculate
     k = args.nb_matches_needed  # number of needed matches
-    just_eq_matrix = 1
+    just_eq_matrix = 0
     vec_size = 1024  # vector size
     t = args.same_t
     q = args.nb_queries
-    r = 0.85#math.floor(t * n)
-    c = 50/85#args.diff_t * (n / r)
+    r = 0.9#math.floor(t * n)
+    c = 50/90#args.diff_t * (n / r)
     s = args.lsh_size
 
 
@@ -94,7 +137,7 @@ if __name__ == '__main__':
     if args.dataset == "rand" or args.dataset == "all":
 
         t_start = time.time()
-        random_data = build_rand_dataset(M, vec_size, t)
+        random_data, queries = build_rand_dataset(M, vec_size, t)
         t_end = time.time()
         t_dataset = t_end - t_start
         print("Successfully generated random data in "+str(t_dataset)+" seconds")
@@ -103,7 +146,8 @@ if __name__ == '__main__':
         # print("random data size : ", len(random_data))
 
         data = to_iris(random_data)
-        query = to_iris(random_data[0])
+        queries = to_iris(queries)
+        # query = to_iris(random_data[0])
 
         success = 1
         counter = 0
@@ -123,6 +167,7 @@ if __name__ == '__main__':
                 eLSH = eLSH_import.eLSH(LSH, vec_size, r, c, s, n)
                 lsh = eLSH.hashes
                 lsh_list = compute_eLSH(data)
+                queries_lsh_list = compute_eLSH(queries)
                 t_end = time.time()
                 t_lsh = t_end - t_start
                 print("Successfully generated LSH evaluations in "+str(t_lsh)+" seconds")
@@ -130,7 +175,7 @@ if __name__ == '__main__':
                 # print("******************")
                 #print(len(lsh_list[0]), len(lsh_list))
                 t_start = time.time()
-                eq = gen_eq_matrix(len(lsh_list), len(lsh_list[0]), lsh_list, False)
+                eq = gen_eq_matrix(len(lsh_list), len(lsh_list[0]), lsh_list)
                 t_end = time.time()
                 t_eq = t_end - t_start
                 print("Successfully generated equality matrix in " + str(t_eq) + " seconds")
@@ -151,13 +196,14 @@ if __name__ == '__main__':
         t_end = time.time()
         t_code_sampling = t_end - t_start
         print("Successfully sampled codes in " + str(t_code_sampling) + " seconds")
-        print(codes)
 
         dict = gen_dict(codes, M, n, lsh_list)
 
         #l_query = compute_eLSH(query)
-        l_query = lsh_list[1]
-        print(search_query_dict(l_query, lsh_list, k, dict))
+        #l_query = lsh_list[1]
+
+        for j in range(len(queries_lsh_list)):
+            print(j, search_query_dict(queries_lsh_list[j], lsh_list, k, dict))
 
 
 
