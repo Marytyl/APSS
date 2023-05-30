eb899ee5cc898da2124f380b73fc381e6d9c2b33
Maryam Rezapour <maryamrezapour+github@gmail.com>
2023-05-30 11:08:01 -0400

    added new arg for error rate to the script

--- params_million.sh
+++ params_million.sh
@@ -7,11 +7,15 @@ done
 python3 main.py --dataset_size=1000000 --lsh_size=31 --nb_eLSHes=10738 --nb_matches_needed=25 > million85.txt
 
 
-python3 main.py --dataset_size=10000 --lsh_size=24 --nb_eLSHes=5012 --nb_matches_needed=33 --eps_t=85 --eps_f=50  > tenthousand85c15.txt
-python3 main.py --dataset_size=10000 --lsh_size=21 --nb_eLSHes=631 --nb_matches_needed=22 --eps_t=90 --eps_f=50  > tenthousand9c15.txt
+python3 main.py --dataset_size=10000 --lsh_size=24 --nb_eLSHes=5012 --nb_matches_needed=33 --eps_t=85 --eps_f=50 --error_rate_percent=15
+ > tenthousand85c15.txt
+python3 main.py --dataset_size=10000 --lsh_size=21 --nb_eLSHes=631 --nb_matches_needed=22 --eps_t=90 --eps_f=50 --error_rate_percent=10
+ > tenthousand9c15.txt
 
-python3 main.py --dataset_size=10000 --lsh_size=23 --nb_eLSHes=3982 --nb_matches_needed=29 --eps_t=85 --eps_f=50  > tenthousand85c13.txt
-python3 main.py --dataset_size=10000 --lsh_size=21 --nb_eLSHes=1000 --nb_matches_needed=34 --eps_t=90 --eps_f=50  > tenthousand9c13.txt
+python3 main.py --dataset_size=10000 --lsh_size=23 --nb_eLSHes=3982 --nb_matches_needed=29 --eps_t=85 --eps_f=50 --error_rate_percent=15
+  > tenthousand85c13.txt
+python3 main.py --dataset_size=10000 --lsh_size=21 --nb_eLSHes=1000 --nb_matches_needed=34 --eps_t=90 --eps_f=50 --error_rate_percent=10
+  > tenthousand9c13.txt
 
 
 
